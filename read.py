fe8d85240e3483c890991235781fcdee15599236
Abhiram <abhiramreddymuthyala3401@gmail.com>
2025-04-23 18:26:30 +0530

    commit 1.1

--- read.py
+++ read.py
@@ -1,11 +1,13 @@
 # https://chatgpt.com/c/68079c7e-78c0-8003-afdf-9eab3142d3fc
 import cv2
 img = cv2.imread(r"c:\Users\abhir\Downloads\e6dacfc4-3343-4cf8-82f5-8cf2e409e601_2.jpeg")
-cv2.imshow('image',img)
-cv2.waitKey(5000)
-print(type(img))
 print(img.shape)
-print(img[0].shape)
+resized = cv2.resize(img, (800, 800)) #(widht, height)
+cv2.imshow('image',resized)
+cv2.waitKey(5000)
+# print(type(img))
+# print(img.shape)
+# print(img[0].shape)
 
 # vid_path = r"C:\Users\abhir\Videos\2024-08-08 21-39-07.mkv"
 # video = cv2.VideoCapture(vid_path)
