59738e81513ab30c7833951a7796ac2d8d1685d5
Abhiram <abhiramreddymuthyala3401@gmail.com>
2025-04-25 19:55:51 +0530

    commmit 1.3

--- read.py
+++ read.py
@@ -10,30 +10,33 @@ img = cv2.imread(r"c:\Users\abhir\Downloads\e6dacfc4-3343-4cf8-82f5-8cf2e409e601
 # print(img.shape)
 # print(img[0].shape)
 #CREATING DIFFRENT NAMED WINDOWS
-cv2.imshow('actual',img)
-cv2.waitKey(3000)
-cv2.destroyAllWindows()  # if i use this line then the acutal image image will be destroyed and the later iamges are visible on the screen , if not one problem will arise , check by commenting this line 
+# cv2.imshow('actual',img)
+# cv2.waitKey(3000)
+# cv2.destroyAllWindows()  # if i use this line then the acutal image image will be destroyed and the later iamges are visible on the screen , if not one problem will arise , check by commenting this line 
 
-cv2.namedWindow('Normal window', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('Normal window', 800, 300)
+# cv2.namedWindow('Normal window', cv2.WINDOW_NORMAL)
+# cv2.resizeWindow('Normal window', 800, 300)
 
-cv2.namedWindow('Keepratio',cv2.WINDOW_KEEPRATIO)
-cv2.resizeWindow('Keepratio',400,550)
+# cv2.namedWindow('Keepratio',cv2.WINDOW_KEEPRATIO)
+# cv2.resizeWindow('Keepratio',400,550)
 
-cv2.imshow('Normal window', img)
-cv2.waitKey(3000)
-cv2.imshow('keepratio', img)
-cv2.waitKey(3000)
-cv2.destroyAllWindows()
-
-
-# vid_path = r"C:\Users\abhir\Videos\2024-08-08 21-39-07.mkv"
-# video = cv2.VideoCapture(vid_path)
-# ret = True
-# while ret:
-#     ret , frame = video.read()
-#     if ret:
-#         cv2.imshow('vidoe',frame)
-#         cv2.waitKey(40)
-# video.release()
+# cv2.imshow('Normal window', img)
+# cv2.waitKey(3000)
+# cv2.imshow('keepratio', img)
+# cv2.waitKey(3000)
 # cv2.destroyAllWindows()
+
+
+vid_path = r"C:\Users\abhir\Pictures\Camera Roll\WIN_20250424_21_33_39_Pro.mp4"
+video = cv2.VideoCapture(vid_path)
+print(type(video))
+ret = True
+cv2.namedWindow('video window',cv2.WINDOW_FULLSCREEN)
+# cv2.resizeWindow('video window',800,500)
+while ret:
+    ret , frame = video.read()
+    if ret:
+        cv2.imshow('video window',frame)
+        cv2.waitKey(40)
+video.release()
+cv2.destroyAllWindows()
